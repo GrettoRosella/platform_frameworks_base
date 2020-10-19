yaap/frameworks_base

base: Allow hiding auto brightness toggle [1/2]
Change-Id: I8abcf0a2c9dc25fd70702352ad9cccc405d53920

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -101,6 +101,7 @@ public class SystemSettings {
         Settings.System.QS_FOOTER_TEXT_STRING,
         Settings.System.QS_SHOW_BRIGHTNESS,
         Settings.System.QS_BRIGHTNESS_POSITION_BOTTOM,
+        Settings.System.QS_SHOW_AUTO_BRIGHTNESS,
         Settings.System.NOTIFICATION_HEADERS,
         Settings.System.TORCH_LONG_PRESS_POWER_GESTURE,
         Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT,

GrettoRosella/platform_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -88,6 +88,6 @@ public class SystemSettings {
         Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN,
         Settings.System.QS_SHOW_BRIGHTNESS,
         Settings.System.QS_BRIGHTNESS_POSITION_BOTTOM,
-        Settings.System.LOCKSCREEN_MEDIA_ART
-    };
+        Settings.System.LOCKSCREEN_MEDIA_ART,
+        Settings.System.QS_SHOW_AUTO_BRIGHTNESS
 }